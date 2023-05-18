7715c398629787828c4c181566428aa7f63bb355
NMTuan <nmtuan@qq.com>
2023-05-18 09:52:43 +0800

    chore: 统一 response code

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { ref } from 'vue';
 import axios from 'axios';
 
 // axios 实例
@@ -10,21 +9,12 @@ let pending = []  // 队列
 let isPending = false
 
 const handlerFetch = () => {
-  // console.log('[handlerFetch]')
-  // /test
   instance.get('/test', { needToken: true })
-    .then(res => {
-      console.log('123', res)
-    })
-    .catch((err) => {
-      console.error('[error]', err)
-    })
 }
 const handlerLogin = () => {
-  console.log('[handlerLogin]')
   instance.post('/login')
     .then((response) => {
-      if (response.data.code === 200) {
+      if (response.data.code === 20000) {
         localStorage.setItem('token', response.data.data.access_token)
         localStorage.setItem('refresh_token', response.data.data.refresh_token)
       }
@@ -36,7 +26,6 @@ const handlerLogin = () => {
 }
 const handlerRefresh = () => {
   instance.post('/refresh')
-  // /refresh
 }
 
 const refreshToken = () => {
@@ -69,11 +58,12 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   (response) => {
     const { code } = response.data
-    if (code === 401) {
+    // 40100 用户身份验证失败
+    if (code === 40100) {
       const originalConfig = response.config; // 保存下请求信息
-
+      // 判断队列
       if (isPending) {
-        console.log('已经有刷新请求了，当前请求加入队列')
+        console.log('已经有刷新请求了，先加到队列等着吧。。。')
 
         return new Promise((resolve) => {
           pending.push((token) => {
@@ -83,27 +73,26 @@ instance.interceptors.response.use(
           })
         })
       } else {
-        console.log('请求401了，开始刷新token')
+        console.log('请求40100了，开始刷新token')
         isPending = true
         return refreshToken()
           .then((response) => {
             console.log('刷新token返回内容', response)
-            // todo 这里需要判断刷新请求返回的状态
-            if (response.data.code !== 200) {
+            if (response.data.code === 20000) {
+              localStorage.setItem('token', response.data.data.access_token)
+              localStorage.setItem('refresh_token', response.data.data.refresh_token)
+              // 处理队列
+              pending.forEach((cb) => cb(response.data.data.refresh_token))
+              // 清空队列
+              pending = []
+              // 带着新token重新发起当前请求
+              originalConfig.headers['Authorization'] = response.data.data.refresh_token
+              return instance(originalConfig)
+            } else if (response.data.code === 40101) {
               console.log('刷新token也过期了，重新登录吧！')
               return response.data
             }
-            localStorage.setItem('token', response.data.data.access_token)
-            localStorage.setItem('refresh_token', response.data.data.refresh_token)
-            // 处理队列
-            pending.forEach((cb) => cb(response.data.data.refresh_token))
-            // 清空队列
-            pending = []
 
-            // 强制清一下？
-            // originalConfig.baseURL = ''
-            originalConfig.headers['Authorization'] = response.data.data.refresh_token
-            return instance(originalConfig)
           })
           .catch((error) => {
             console.error(error)
@@ -137,11 +126,3 @@ instance.interceptors.response.use(
     </div>
   </div>
 </template>
-
-<style >
-html,
-body {
-  background-color: #000;
-  color: #ccc;
-}
-</style>
